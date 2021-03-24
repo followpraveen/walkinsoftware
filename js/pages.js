3ae7bd521bee0bd58b4ece5eb48fe881daf1164d
followpraveen <you@example.com>
2021-03-24 13:31:38 +0530

    complete website

---
+++ js/pages.js
@@ -0,0 +1,29 @@
+$('.btn').click(function() {
+	var url = $(this).attr('data-url');
+	window.open(url);
+});
+
+// slider
+
+$('#recipeCarousel').carousel({
+	interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+	  var minPerSlide = 3;
+	  var next = $(this).next();
+	  if (!next.length) {
+	  next = $(this).siblings(':first');
+	  }
+	  next.children(':first-child').clone().appendTo($(this));
+	  
+	  for (var i=0;i<minPerSlide;i++) {
+		  next=next.next();
+		  if (!next.length) {
+			  next = $(this).siblings(':first');
+			}
+		  
+		  next.children(':first-child').clone().appendTo($(this));
+		}
+  });
+  
---
+++ js/seo.js
@@ -0,0 +1,18 @@
+
+
+setTimeout(function() {
+  $('#basicExampleModal').modal('show');
+},3500)
+
+
+// Only number in input field
+
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
